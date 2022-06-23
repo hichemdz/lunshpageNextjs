1ba49a37915af1fddc4fdba6e4c8cd03fc96cb1b
lakhdar <hichem.oued@gmail.com>
2022-06-23 09:27:40 +0100

    add google tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
-import "slick-carousel/slick/slick.css"; 
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+import "../styles/globals.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-PT3XXS6' });
+}, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
